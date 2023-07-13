fa9f915d9f177fc77165e31693db8682feebc61e
nadamansour7 <nadamansour5721@gmail.com>
2023-07-13 15:30:23 -0700

    my first commit in c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (success)
 */
+
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art, \n");
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
