c4a1856c9fe9b4240c0a2e0404077ad5a64daa0e
nadamansour7 <nadamansour5721@gmail.com>
2023-07-13 16:14:31 -0700

    commit_3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,17 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-=======
 #include<stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
- */
+ * Description: using sixeof to print the size of various types.
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
-{ 
-       printf("size of a char: %d byte(s)\n", sizeof(char));
-       printf("size of an int: %d byte(s)\n", sizeof(int));
-       printf("size of a long int: %d byte(s)\n", sizeof(long int));
-       printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-       printf("size of a float: %d byte(s)\n", sizeof(float));
-       return (0);
+{
+	printf("size of a char: %lu byte(s)\n", sizeof(char));
+	printf("size of an int: %lu byte(s)\n", sizeof(int));
+	printf("size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %lu byte(s)\n", sizeof(float));
+	return (0);
 }
->>>>>>> 40c312b0f23606d4fb105edab17ae8d13a8ab580
