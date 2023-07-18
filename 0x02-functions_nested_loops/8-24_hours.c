be95064093c61d4d78e80b7f49760447c7baedfd
nadamansour7 <nadamansour5721@gmail.com>
2023-07-18 15:36:48 -0700

    n

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,23 +5,18 @@
  */
 void jack_bauer(void)
 {
-	int i, j;
+	int min, hr;
 
-	i = 0;
-
-	while (i < 24)
+	for (hr = 0; hr <= 23; hr++)
 	{
-		j = 0;
-		while (j < 60)
+		for (min = 0; min <= 59; min++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar((':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
-			j++;
 		}
-		i++;
 	}
 }
